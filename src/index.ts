6eb8e1d5bb5993d5a89db0fd6143d467a0289b36
Giuliano Gabella <ggabella91@gmail.com>
2022-07-14 22:52:14 -0500

    Renamed middleware directory from plural to singular form

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,10 @@ export * from './errors/not-found-error';
 export * from './errors/request-validation-error';
 export * from './errors/internal-server-error';
 
-export * from './middlewares/current-user';
-export * from './middlewares/error-handler';
-export * from './middlewares/require-auth';
-export * from './middlewares/validate-request';
+export * from './middleware/current-user';
+export * from './middleware/error-handler';
+export * from './middleware/require-auth';
+export * from './middleware/validate-request';
 export * from './events/base-listener';
 export * from './events/base-publisher';
 export * from './events/subjects';
